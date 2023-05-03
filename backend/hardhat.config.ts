bb1715e0e50710c3b8a04963fefef93f603323a3
Zstzbxn <zstzbxn@pop-os.localdomain>
2023-05-03 17:09:46 +0200

    change goerli for sepolia

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -5,14 +5,14 @@ import "@typechain/hardhat"
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   defaultNetwork: "hardhat",
   networks: {
-    goerli: {
-      url: GOERLI_RPC_URL,
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 5,
     },
